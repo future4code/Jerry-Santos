60aa019d9a0bed9d379fcb30c229fb18b9442ed7
Jerry Jones <jerryjfernandes@gmail.com>
2020-04-07 15:09:54 -0300

    Exercício 2 resolvido

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -4,3 +4,9 @@ e converte para reais. A função recebe o valor da cotação em um prompt, cert
 de transformar ele pro tipo number e multiplica pelo valor informado em dólares.
 No exemplo da imagem se a cotação do dólar estivesse em 5.20, ele retornaria
 no console R$520 */
+
+/* 2. Na função que simula um programa de investimentos é passado como parâmetro
+o tipo de investimento e o valor. Um switch case vai analisar o parâmetro
+tipoDeInvestimento para que possa fazer o cálculo adicionando a uma variável
+que guarda esse valor para dar o retorno da função. Nos exemplos do código a resposta
+do console é 161 e um alerta na tela informando que o tipo de investimento está incorreto. */
