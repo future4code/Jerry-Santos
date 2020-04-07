b8e911928ccf08b72ad4e1d41850f6650fdd806f
Jerry Jones <jerryjfernandes@gmail.com>
2020-04-07 15:09:09 -0300

    Exercício 1 resolvido

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,6 @@
+// Exercícios de leitura de código
+/* 1. Um conversor de moedas que recebe como parâmetro o valor em dólar 
+e converte para reais. A função recebe o valor da cotação em um prompt, certifica
+de transformar ele pro tipo number e multiplica pelo valor informado em dólares.
+No exemplo da imagem se a cotação do dólar estivesse em 5.20, ele retornaria
+no console R$520 */
