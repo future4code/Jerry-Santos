3cb294413ce60e6d3bc90905f3974dc384b5607b
Jerry Jones <jerryjfernandes@gmail.com>
2020-04-07 15:10:10 -0300

    Exercício 3 resolvido

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -10,3 +10,9 @@ o tipo de investimento e o valor. Um switch case vai analisar o parâmetro
 tipoDeInvestimento para que possa fazer o cálculo adicionando a uma variável
 que guarda esse valor para dar o retorno da função. Nos exemplos do código a resposta
 do console é 161 e um alerta na tela informando que o tipo de investimento está incorreto. */
+
+/* 3. O programa tem um array de número definido e quer criar dois novos arrays
+com números pares e ímpares. As respostas do console serão respectivamente
+"Quantidade total de números 14"
+6
+8 */
