e4a6832789d8de00f78f334bfda39278dc9b4d56
Jerry Jones <jerryjfernandes@gmail.com>
2020-04-14 15:29:05 -0300

    Labedin: exercício do dia resolvido

---
+++ semana5/labedin/build/precache-manifest.96bde5a34b2fd6715961272260464b32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d5ea9cf43ee8924dcfa5a0c1275067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "562a6f9ec480a2f4de3f",
+    "url": "/static/css/main.52ce0cf4.chunk.css"
+  },
+  {
+    "revision": "dd35cab1ec671b13bbed",
+    "url": "/static/js/2.65cf7749.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65cf7749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "562a6f9ec480a2f4de3f",
+    "url": "/static/js/main.e6059e64.chunk.js"
+  },
+  {
+    "revision": "9cec4286d4e96efb4903",
+    "url": "/static/js/runtime-main.6b07746c.js"
+  }
+]);
