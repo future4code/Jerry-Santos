0fdeeb63e7aba4d4fc8decb767aa9460fac39452
Jerry Jones <jerryjfernandes@gmail.com>
2020-04-29 14:36:13 -0300

    Labenusers: Exercício finalizado

--- semana7/labenusers-jj/src/App.js
+++ semana7/labenusers-jj/src/App.js
@@ -3,12 +3,45 @@ import styled from 'styled-components';
 import { ListaUsuarios } from './components/ListaUsuarios';
 import axios from 'axios';
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+` 
+const ContainerUm = styled.div`
+  justify-items: flex-start;
+  margin: 1rem;
+` 
+const ContainerDois = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+`
+
+const ContainerForm = styled.div`
+  width: 25rem;
+  height: 12rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px #2D3058 solid;
+`
+const BotaoForm = styled.button`
+  width: 4rem;
+  height: 2rem;
+  margin-top: 1rem;
+  color: white;
+  background-color: #2D3058;
+  border: none;
+`
 
 
 class App extends React.Component {
   state = {
     lista: [],
-    inicio: true
+    inicio: true,
+    inputNomeValor: "",
+    inputEmailValor: ""
   }
 
   componentDidMount() {
@@ -36,26 +69,61 @@ class App extends React.Component {
     this.setState({inicio: !this.state.inicio})
     this.pegaUsuarios();
   }
+
+  inputName = (event) => {
+    this.setState({inputNomeValor: event.target.value})
+  }
+
+  inputEmail = (event) => {
+    this.setState({inputEmailValor: event.target.value})
+  }
+  
+  
+
+  novoUsuario = () => {
+    const body = {
+      "name": this.state.inputNomeValor,
+      "email": this.state.inputEmailValor
+    }
+    axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", body, {
+      headers: {
+        Authorization: "jerry-jones-julian"
+      }
+    }).then( response => {
+      window.alert("Novo usuário cadastrado")
+      this.setState({inputNomeValor: "", inputEmailValor: ""})
+      this.pegaUsuarios();
+    }).catch( error => {
+      window.alert("Ihh! Algo parece estar errado")
+      this.setState({inputNomeValor: "", inputEmailValor: ""})
+    })
+    
+  }
   
   render() {
     
     const cadastrarUsuarios = (
-      <div>
+      <ContainerForm>
         <label>Nome: </label>
-        <input/>
+        <input value={this.state.inputNomeValor} onChange={this.inputName}/>
         <label>E-mail: </label>
-        <input/>
-        <button>Salvar</button>
-      </div>
+        <input value={this.state.inputEmailValor} onChange={this.inputEmail}/>
+        <BotaoForm onClick={this.novoUsuario}>Salvar</BotaoForm>
+      </ContainerForm>
     )
     
     
     console.log()
     return (
-      <div>
+      <Container>
+        <ContainerUm>
           <button onClick={this.alteraBotao}>{this.state.inicio ? "Ir para página de lista" : "Ir para página de registro"}</button>
+        </ContainerUm>    
+        <ContainerDois>
           {this.state.inicio ? cadastrarUsuarios:<ListaUsuarios listaAtualizada={this.state.lista}/>}
-      </div>
+        </ContainerDois>
+      </Container>
+      
     );
   }
 }
--- semana7/labenusers-jj/src/components/ListaUsuarios.js
+++ semana7/labenusers-jj/src/components/ListaUsuarios.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
+const BotaoDeletar = styled.button`
+    border: none;
+    color: red;
+    margin-left: 5rem;
+    cursor: pointer;
+`   
 
 export class ListaUsuarios extends React.Component {
+
+
+    apagarUsuario = (userId) => {
+        axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${userId}`, {
+        headers: {
+            Authorization: "jerry-jones-julian"
+        }
+
+    }).then( response => {
+        window.alert("Usuário deletado")
+        window.location.reload()
+    }).catch( error => {
+        window.alert("Tente novamente")
+    })
+    
+    }
+
   render() {
-      let listaFinal = this.props.listaAtualizada.map((usuario) => {
-          return (
-            <div>
-              <p>{usuario.name}</p>
+      const listaFinal = this.props.listaAtualizada.map((usuario, index) => {
+        return (
+            <div key={index}>
+              <span>{usuario.name}</span>
+              <BotaoDeletar onClick={() => this.apagarUsuario(usuario.id)}>X</BotaoDeletar>
               <hr/>
             </div>
           )
