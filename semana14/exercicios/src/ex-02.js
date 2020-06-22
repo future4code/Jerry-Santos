51df3cf598b3f66489b9b53f272e160fb86132f9
Jerry Jones <jerryjfernandes@gmail.com>
2020-06-22 16:30:06 -0300

    Exercícios do dia iniciando

---
+++ semana14/exercicios/src/ex-02.js
@@ -0,0 +1,17 @@
+// Exercício 02
+function minhaFuncao(a, b) {
+    // a.
+    console.log(a + b);
+    // b.
+    console.log(a - b);
+    // c.
+    console.log(a * b);
+    // d.
+    if (a > b) {
+        console.log(a);
+    }
+    else {
+        console.log(b);
+    }
+}
+minhaFuncao(7, 4);
