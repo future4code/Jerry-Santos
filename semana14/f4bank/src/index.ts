8d5dd5393be51aa23b70b21a4bdba8af96fad03f
Jerry Jones <jerryjfernandes@gmail.com>
2020-06-26 11:43:03 -0300

    Primeiras linhas ainda sem efeito

--- semana14/f4bank/src/index.ts
+++ semana14/f4bank/src/index.ts
@@ -1,9 +1,10 @@
 import * as fs from 'fs';
+import * as moment from 'moment';
 
 type AccountUser = {
    name: string,
    cpf: number,
-   birthDate: string,
+   birthDate: moment.Moment,
    accountBalance: number,
    accountStatement: any[] 
 }
@@ -13,3 +14,15 @@ type Transaction = {
     date: number,
     description: string
 }
+
+// const createAccount = (user: AccountUser): void => {
+
+// }
+
+// const name: string = process.argv[2];
+// const cpf: number = process.argv[3];
+// const birthDate: moment.Moment =
+
+// const user: AccountUser = {
+
+// }
